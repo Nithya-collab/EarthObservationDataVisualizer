4d5a2cb2810144e75278fe7b6d715b2acbe28a9c
Charles Kumar <gamescharlie79@gmail.com>
2025-11-21 20:59:12 +0530

    fixed: move the sidebar trigger button based on sidebar open state
    * get the sidebar open state from useSidebar hook
    * create a new functional component SidebarInsetContent to be used within SidebarProvider
    * we created a custom component SidebarInsetContent to just get the open state without touching the shadcn code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,29 +4,41 @@ import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
+  useSidebar,
 } from "@/components/ui/sidebar"
 import Leaflet from "./components/ui/library/Leaflet"
 import { ModeToggle } from "./components/ui/mode-toggle"
+import { cn } from "./lib/utils"
+
+function SidebarInsetContent() {
+  const { open } = useSidebar();
+  return (
+    <SidebarInset>
+      <header className="flex absolute top-0 left-12 shrink-0 items-center gap-2 border-b px-4">
+        <SidebarTrigger className={cn("-ml-1 bg-white/45 dark:bg-black/45 z-1",
+          open ? "ml-50" : "-ml-12"
+        )} />
+        <Separator
+          orientation="vertical"
+          className="mr-2 data-[orientation=vertical]:h-4"
+        />
+      </header>
+      <div className="flex">
+        <Leaflet></Leaflet>
+        <ModeToggle className="bg-white/45 dark:bg-black/45! z-2 absolute bottom-5 right-2"></ModeToggle>
+      </div>
+    </SidebarInset>
+  )
+
+}
 
 export default function Page() {
   return (
     <SidebarProvider>
       <AppSidebar />
-      <SidebarInset>
-        <header className="flex absolute top-0 left-12 shrink-0 items-center gap-2 border-b px-4">
-          <SidebarTrigger className="-ml-1 bg-white/45 dark:bg-black/45 z-1" />
-          <Separator
-            orientation="vertical"
-            className="mr-2 data-[orientation=vertical]:h-4"
-          />
-          <ModeToggle></ModeToggle>
-        </header>
-        <div className="flex">
-          <Leaflet></Leaflet>
-        </div>
-      </SidebarInset>
+      <SidebarInsetContent /> 
     </SidebarProvider>
 
-    
+
   )
 }
