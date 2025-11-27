73e34c16ba8c3f11501550f71e30b7e9aa150f2f
charles-prof <68528137+charles-prof@users.noreply.github.com>
2025-11-27 09:25:53 +0000

    feat(models): add Place model for storing spatial data with PostGIS/GeoAlchemy2

---
+++ map_viz/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String
+from geoalchemy2 import Geometry
+from my_app.database import Base # Import our configured Base object
+
+class Place(Base):
+    """
+    A model to store locations with spatial data using PostGIS/GeoAlchemy2.
+    """
+    __tablename__ = 'places'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50), index=True)
+    # Define a Geometry column: POINT type, SRID 4326 (WGS84 lat/lon)
+    geom = Column(Geometry(geometry_type='POINT', srid=4326))
+
+    def __repr__(self):
+        return f"<Place(name='{self.name}', geom='{self.geom}')>"
+        
