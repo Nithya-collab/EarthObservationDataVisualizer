5c0a225308c6485728798435167acabe278c2731
Charles Kumar <gamescharlie79@gmail.com>
2025-12-17 15:16:33 +0530

    config: add config for manifest icon generation
    TODO: satisfy installability criteria

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,13 +3,14 @@ import path from 'path'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
+import { manifestForPlugIn } from './pwa-assets.config'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(), 
     tailwindcss(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA(manifestForPlugIn)
   ],
   resolve: {
     alias: {
